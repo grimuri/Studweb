bc7d88af7d1897d8aaf5efb1a76024fcd66c270b
Jakub Popiołek <grimurin@gmail.com>
2024-12-25 18:45:50 +0100

    Add note domain class

---
+++ src/Core/Studweb.Domain/Aggregates/Note/Note.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Studweb.Domain.Aggregates.Note.ValueObjects;
+using Studweb.Domain.Aggregates.User.ValueObjects;
+using Studweb.Domain.Primitives;
+
+namespace Studweb.Domain.Aggregates.Note;
+
+public sealed class Note : AggregateRoot<NoteId>
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime LastModifiedOnUtc { get; private set; }
+    public List<Tag> Tags { get; set; }
+    public UserId UserId { get; private set; }
+    
+    private Note(NoteId id = default) : base(id)
+    {
+    }
+
+    private Note(
+        NoteId noteId,
+        string title,
+        string content,
+        List<Tag> tags,
+        UserId userId
+    ) : base(noteId)
+    {
+        Title = title;
+        Content = content;
+        CreatedOnUtc = DateTime.UtcNow;
+        LastModifiedOnUtc = DateTime.UtcNow;
+        Tags = tags;
+        UserId = userId;
+    }
+
+    public static Note Create(
+        NoteId? noteId,
+        string title,
+        string content,
+        List<Tag> tags,
+        UserId userId
+    )
+    {
+        if (noteId is null)
+        {
+            noteId = NoteId.Create();
+        }
+
+        var note = new Note(
+            noteId,
+            title,
+            content,
+            tags,
+            userId);
+
+        return note;
+    }
+}
---
+++ src/Core/Studweb.Domain/Aggregates/Note/ValueObjects/Tag.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Studweb.Domain.Primitives;
+
+namespace Studweb.Domain.Aggregates.Note.ValueObjects;
+
+public sealed class Tag : ValueObject
+{
+    public string Value { get; }
+
+    [JsonConstructor]
+    private Tag(string value)
+    {
+        Value = value;
+    }
+
+    public static Tag Create(string value) => new Tag(value);
+    
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
