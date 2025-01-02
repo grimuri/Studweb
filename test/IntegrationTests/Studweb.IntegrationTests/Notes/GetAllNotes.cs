32d46a142baa62a5ccf23da0ad804a7061857a57
Jakub Popiołek <grimurin@gmail.com>
2025-01-02 13:37:57 +0100

    Add integration tests to the GetAllNotes functionality

---
+++ test/IntegrationTests/Studweb.IntegrationTests/Notes/GetAllNotes.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Studweb.Application.Features.Notes.Queries.GetAllNotes;
+using Studweb.Domain.Common.Errors;
+using Studweb.IntegrationTests.Abstractions;
+
+namespace Studweb.IntegrationTests.Notes;
+
+[Collection("IntegrationTests")]
+public class GetAllNotes : BaseIntegrationTest
+{
+    public GetAllNotes(IntegrationTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnErrorUserNotAuthenticated_WhenUserIsNotAuthenticated()
+    {
+        // Arrange
+        
+        var getAllNotesQuery = new GetAllNotesQuery();
+        HttpContextAccessor.HttpContext = null;
+        
+        // Act
+
+        var result = await Sender.Send(getAllNotesQuery);
+        
+        // Assert
+        
+        result.IsError.Should().BeTrue();
+        result.Errors.Should().Contain(Errors.User.UserNotAuthenticated);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnAllNotesBelongingToUser_WhenUserIsAuthenticated()
+    {
+        // Arrange
+
+        var getAllNotesQuery = new GetAllNotesQuery();
+        
+        // Act
+
+        var result = await Sender.Send(getAllNotesQuery);
+        
+        // Assert
+
+        result.IsError.Should().BeFalse();
+        result.Value.Notes.Should().HaveCountGreaterThan(-1);
+    }
+}
