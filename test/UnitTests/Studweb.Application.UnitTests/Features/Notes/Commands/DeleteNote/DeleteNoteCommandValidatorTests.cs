af9980c31903e7c5c8c95af8628ecdb7582f2bff
Jakub Popiołek <grimurin@gmail.com>
2025-01-03 19:47:03 +0100

    Add unit tests to DeleteNoteCommandValidator

---
+++ test/UnitTests/Studweb.Application.UnitTests/Features/Notes/Commands/DeleteNote/DeleteNoteCommandValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using Studweb.Application.Features.Notes.Commands.DeleteNote;
+
+namespace Studweb.Application.UnitTests.Features.Notes.Commands.DeleteNote;
+
+public class DeleteNoteCommandValidatorTests
+{
+    private readonly DeleteNoteCommandValidator _deleteNoteCommandValidator = new();
+    
+    [Theory]
+    [InlineData(-5)]
+    [InlineData(0)]
+    [InlineData(null)]
+    public async Task Id_Should_NotBeEmpty(int id)
+    {
+        // Arrange
+
+        var deleteNoteCommand = new DeleteNoteCommand(id);
+        
+        // Act
+
+        var result = await _deleteNoteCommandValidator.TestValidateAsync(deleteNoteCommand);
+        
+        // Assert
+
+        result.ShouldHaveValidationErrorFor(x => x.Id);
+    }
+}
