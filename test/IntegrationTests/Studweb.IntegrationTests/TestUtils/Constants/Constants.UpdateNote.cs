e21a7ab6b611a6a17d948a6ff775967433922d92
Jakub Popiołek <grimurin@gmail.com>
2025-01-03 17:30:42 +0100

    Add integration tests to update note functionality

---
+++ test/IntegrationTests/Studweb.IntegrationTests/TestUtils/Constants/Constants.UpdateNote.cs
@@ -0,0 +1,20 @@
+﻿using Studweb.Domain.Aggregates.Notes.ValueObjects;
+
+namespace Studweb.IntegrationTests.TestUtils.Constants;
+
+public static partial class Constants
+{
+    public static class UpdateNote
+    {
+        public const int Id = 1;
+        public const string Title = "NewTitle";
+        public const string Content = "NewContent";
+
+        public static readonly List<Tag> Tags = new()
+        {
+            Tag.Create("Tag12"),
+            Tag.Create("Tag22"),
+            Tag.Create("Tag32")
+        };
+    }
+}
