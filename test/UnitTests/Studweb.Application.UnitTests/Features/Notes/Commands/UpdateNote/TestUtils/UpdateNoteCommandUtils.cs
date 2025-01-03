91eb9cbd8a00148a7f99c7dd1e36ddb24dea0051
Jakub Popiołek <grimurin@gmail.com>
2025-01-03 17:03:10 +0100

    Add unit tests to UpdateNoteCommandHandler

---
+++ test/UnitTests/Studweb.Application.UnitTests/Features/Notes/Commands/UpdateNote/TestUtils/UpdateNoteCommandUtils.cs
@@ -0,0 +1,14 @@
+﻿using Studweb.Application.Features.Notes.Commands.UpdateNote;
+using Studweb.Application.UnitTests.TestUtils.Constants;
+
+namespace Studweb.Application.UnitTests.Features.Notes.Commands.UpdateNote.TestUtils;
+
+public static class UpdateNoteCommandUtils
+{
+    public static UpdateNoteCommand UpdateNoteCommand() =>
+        new UpdateNoteCommand(
+            Constants.UpdateNote.Id,
+            Constants.UpdateNote.Title,
+            Constants.UpdateNote.Content,
+            Constants.UpdateNote.Tags);
+}
